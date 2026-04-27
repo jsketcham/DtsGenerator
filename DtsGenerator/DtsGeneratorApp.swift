4cc9a7a2f828eb215749bac1a83d127d5bc23ef8
jsketcham <jsketcham@earthlink.net>
2026-04-27 12:06:57 -0700

    Initial Commit

---
+++ DtsGenerator/DtsGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DtsGeneratorApp.swift
+//  DtsGenerator
+//
+//  Created by Jim on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct DtsGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
